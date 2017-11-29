ad5dd6507aa4b308f605a08468134877d9e4bd8c
dslaveykov <dslaveykov@axway.com>
2017-11-29 17:26:11 +0200

    Add expand all and collapse all buttons and do not show +/- sign on elements without child pages

--- scripts/ats.js
+++ scripts/ats.js
@@ -16,11 +16,15 @@ function expand_or_collapse_ul_element(expand_sign_element) {
 	var collapse = $(expand_sign_element).text() == "-";
 	var ul_level = parseInt($(expand_sign_element).parent().parent().attr('level')) + 1;
 	if (collapse) {
-		$(expand_sign_element).text('+');
+		if ( $(expand_sign_element).text() == '-' ) {
+			$(expand_sign_element).text('+');
+		}
 		$(expand_sign_element).nextAll('ul[level='+ul_level+']').toggle();
-	} 
+	}
 	else {
-		$(expand_sign_element).text('-');
+		if ( $(expand_sign_element).text() == '+' ) {
+			$(expand_sign_element).text('-');
+		}
 		$(expand_sign_element).nextAll('ul[level='+ul_level+']').toggle();
 	}
 }
@@ -29,7 +33,27 @@ function show_tooltip_for_expand_element(expand_sign_element) {
 	if ( $(expand_sign_element).text() == '-' ) {
 		$(expand_sign_element).attr('title','Collapse');
     }
-    else {
+    else if ( $(expand_sign_element).text() == '+' ) {
     	$(expand_sign_element).attr('title','Expand');
-    }    
+    }
+}
+
+function expand_all() {
+	$('#content').find('p.expand_sign').each(function(idx,itm){
+		if ( $(itm).text() == '+' ) {
+			expand_or_collapse_ul_element(itm);
+		}
+	});
+}
+
+
+function collapse_all() {
+	$('#content').find('p.expand_sign').each(function(idx,itm){
+		if ( $(itm).text() == '-' ) {
+			expand_or_collapse_ul_element(itm); // close all
+			if ( $(itm).parent().parent().attr('level') == 0 ) {
+				expand_or_collapse_ul_element(itm); // open only level one elements
+			}
+		}
+	});
 }
