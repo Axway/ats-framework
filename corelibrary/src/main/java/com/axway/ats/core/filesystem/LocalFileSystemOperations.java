22a9f3e27a0da0cafe719c4347ee88dbc0663a11
Radoslav Kostadinov <rkostadinov@axway.com>
2018-02-28 19:26:28 +0200

    Add missing import

--- corelibrary/src/main/java/com/axway/ats/core/filesystem/LocalFileSystemOperations.java
+++ corelibrary/src/main/java/com/axway/ats/core/filesystem/LocalFileSystemOperations.java
@@ -62,6 +62,7 @@ import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
 import java.util.regex.Pattern;
