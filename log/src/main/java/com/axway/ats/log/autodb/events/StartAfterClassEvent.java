e6a9f7f24e53db2f07ea52863dbacce7e584bb4f
dslaveykov <dslaveykov@axway.com>
2019-03-13 15:12:35 +0200

    [TestNG][Log] Fix LifeCycle state for EndAfterClassEvent and StartAfterClassEvent - both events can be executed if RUN is started

--- log/src/main/java/com/axway/ats/log/autodb/events/StartAfterClassEvent.java
+++ log/src/main/java/com/axway/ats/log/autodb/events/StartAfterClassEvent.java
@@ -31,7 +31,7 @@ public class StartAfterClassEvent extends AbstractLoggingEvent {
     @Override
     protected LifeCycleState getExpectedLifeCycleState( LifeCycleState state ) {
 
-        return LifeCycleState.ATLEAST_SUITE_STARTED;
+        return LifeCycleState.ATLEAST_RUN_STARTED;
     }
 
 }
