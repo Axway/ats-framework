e2b57654152d3b9588383e244e91d62259d1fbf5
dslaveykov <dslaveykov@axway.com>
2019-11-14 11:39:41 +0200

    [ExecutorUtils] Fix compilation problem - Use the new system property for the current user's directory

--- agent/webapps/client/src/main/java/com/axway/ats/agent/webapp/client/ExecutorUtils.java
+++ agent/webapps/client/src/main/java/com/axway/ats/agent/webapp/client/ExecutorUtils.java
@@ -45,7 +45,7 @@ public class ExecutorUtils {
             // check if new unique id must be generated each time
             if (!useNewUuId) {
                 // create temp file containing caller working directory and the unique id
-                String userWorkingDirectory = AtsSystemProperties.SYSTEM_USER_HOME_DIR;
+                String userWorkingDirectory = AtsSystemProperties.USER_CURRENT_DIR;
                 String uuiFileLocation = AtsSystemProperties.SYSTEM_USER_TEMP_DIR
                                          + AtsSystemProperties.SYSTEM_FILE_SEPARATOR + "ats_uid.txt";
                 File uuiFile = new File(uuiFileLocation);
