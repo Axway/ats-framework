5a93b2905abaf6ebeafd7bc7e5213a855dbb238a
vvachev-axway <vvachev@axway.com>
2023-09-11 14:55:33 +0300

    Implement FileTransferFtpsClient

--- actionlibrary/src/main/java/com/axway/ats/action/ftp/FileTransferFtpClient.java
+++ actionlibrary/src/main/java/com/axway/ats/action/ftp/FileTransferFtpClient.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Axway Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.axway.ats.action.ftp;
 
 import com.axway.ats.common.PublicAtsApi;
@@ -307,7 +322,6 @@ public class FileTransferFtpClient extends AbstractFileTransferClient implements
                 + client.getPassiveHost());
     }
 
-    @PublicAtsApi
     @Override
     public String[] getAllReplyLines() {
 
@@ -315,7 +329,6 @@ public class FileTransferFtpClient extends AbstractFileTransferClient implements
 
     }
 
-    @PublicAtsApi
     @Override
     public void logAllReplyLines() {
 
@@ -323,7 +336,6 @@ public class FileTransferFtpClient extends AbstractFileTransferClient implements
 
     }
 
-    @PublicAtsApi
     @Override
     public String getAllReplyLinesAsString() {
 
