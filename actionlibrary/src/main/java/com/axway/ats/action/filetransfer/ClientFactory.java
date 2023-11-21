ad5e16a120e559310ce27c28811854d3e83c260f
svilen-d <svilen-d@users.noreply.github.com>
2023-11-21 15:12:28 +0200

    [ActionLibray] Messages update

--- actionlibrary/src/main/java/com/axway/ats/action/filetransfer/ClientFactory.java
+++ actionlibrary/src/main/java/com/axway/ats/action/filetransfer/ClientFactory.java
@@ -126,12 +126,12 @@ public class ClientFactory {
     /**
      * Returns a product specific {@link IFileTransferClient} that handles transfers via the specified {@link TransferProtocol}.
      * 
-     * @param protocol the {@link TransferProtocol} to use
+     * @param customFileTransferClient the class name of the custom client. Used when default implementations in
+     *     {@link TransferProtocol} are not enough
      * @param port a custom port to use when connecting
-     * @param customFileTransferClient the class name of the custom client
      * @return the {@link IFileTransferClient} that handles transfers via the specified {@link TransferProtocol}
-     * @throws FileTransferException 
-     * @see {@link TransferProtocol}
+     * @throws FileTransferException in case of instantiation error
+     * @see com.axway.ats.common.filetransfer.TransferProtocol
      */
     public IFileTransferClient getClient( String customFileTransferClient,
                                           int port ) throws FileTransferException {
