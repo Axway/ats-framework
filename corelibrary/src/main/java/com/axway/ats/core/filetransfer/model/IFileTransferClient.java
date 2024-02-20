0f1ad98084a1045a805512b15633d53d83c7ff61
svilen-d <svilen-d@users.noreply.github.com>
2024-02-20 14:31:59 +0200

    Update license header for recent modifications

--- corelibrary/src/main/java/com/axway/ats/core/filetransfer/model/IFileTransferClient.java
+++ corelibrary/src/main/java/com/axway/ats/core/filetransfer/model/IFileTransferClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Axway Software
+ * Copyright 2017-2023 Axway Software
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
